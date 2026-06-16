f82eba7028d17274b8eade504e7f37532d1d6a73
nelrafa103 <nelcidodiaz13@gmail.com>
2026-06-16 14:07:17 -0400

    feat: agregando scaffold de productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_16_175659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "products", force: :cascade do |t|
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.integer "minimum"
+    t.string "name"
+    t.decimal "price"
+    t.uuid "sku"
+    t.string "status"
+    t.datetime "updated_at", null: false
+  end
 end
